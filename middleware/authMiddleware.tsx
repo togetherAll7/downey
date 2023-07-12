f9504b381bea930ea2838af124a00f8e0df481fd
togetherAll7 <togetheryou7@gmail.com>
2023-07-11 19:49:25 -0700

    fixed some more build issues related to "encoding" missing

--- middleware/authMiddleware.tsx
+++ middleware/authMiddleware.tsx
@@ -6,8 +6,10 @@ const useAuthMiddleware = () => {
   const { state } = useStateContext();
   const router = useRouter();
   const pathName = usePathname();
-  const localUser = localStorage.getItem('user');
-  const localSession = localStorage.getItem('session');
+  const localUser =
+    typeof localStorage !== 'undefined' && localStorage.getItem('user');
+  const localSession =
+    typeof localStorage !== 'undefined' && localStorage.getItem('session');
   console.log('pathName', pathName);
 
   useEffect(() => {
